c259c1ea3f9b5294a6904fab04972bd2a546057b
Aaron Timperman <aaron@appsolu.be>
2022-09-15 23:04:24 +0200

    Correct settings

--- src/settings.ts
+++ src/settings.ts
@@ -1,12 +1,12 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'HealthBoxHomebridgePlugin';
+export const PLATFORM_NAME = 'HealthBoxHomebridgePluginV2';
 
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-healthbox-plugin';
+export const PLUGIN_NAME = 'homebridge-healthbox-plugin-v2';
 
 
 export const HEALTH_VERSION = 2;
